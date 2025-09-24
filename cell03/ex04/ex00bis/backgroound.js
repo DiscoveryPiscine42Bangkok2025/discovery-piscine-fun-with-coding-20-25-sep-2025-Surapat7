8d30275a943cb6ecfc95301f4ae38fcca03d1b73
Softx. <68100500@kmitl.ac.th>
2025-09-24 23:53:51 +0700

    Update backgroound.js

--- cell03/ex04/ex00bis/backgroound.js
+++ cell03/ex04/ex00bis/backgroound.js
@@ -1,16 +1,10 @@
-// โค้ดนี้จะทำงานเมื่อเอกสาร HTML โหลดเสร็จเรียบร้อยแล้ว
-$(document).ready(function(){
-    
-    // ฟังก์ชันสำหรับสร้างโค้ดสี Hex แบบสุ่ม
-    function getRandomHexColor() {
-        const randomColor = Math.floor(Math.random() * 16777215);
-        const hexColor = '#' + randomColor.toString(16).padStart(6, '0');
-        return hexColor;
-    }
+function randomColor() {
+    const p = Math.floor(Math.random() * 256);
+    const q = Math.floor(Math.random() * 256);
+    const r = Math.floor(Math.random() * 256);
+    return `rgb(${p}, ${q}, ${r})`;
+}
 
-    // เมื่อคลิกที่ปุ่ม (#colorChangeBtn)
-    $("#colorChangeBtn").on("click", function() {
-        // เปลี่ยนสีพื้นหลังของ body ให้เป็นสีที่สุ่มมา
-        $("body").css("background-color", getRandomHexColor());
-    });
+document.getElementById("btn").addEventListener("click", function () {
+    document.body.style.backgroundColor = randomColor();
 });
